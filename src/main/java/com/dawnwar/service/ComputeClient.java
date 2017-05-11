4cd31d2b0b1c1befdd48eb68d610b61711f47f9b
lizebei <lizebei@LIZEBEI.ds.gome.com.cn>
2017-05-11 16:14:49 +0800

    create

---
+++ src/main/java/com/dawnwar/service/ComputeClient.java
@@ -0,0 +1,14 @@
+package com.dawnwar.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("client-service")
+public interface ComputeClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+
+}
